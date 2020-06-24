6daf4e1127cc8444281b22165a635a1bc3450086
Taiki Kimura <s1922074@stu.musashino-u.ac.jp>
2020-06-24 14:47:39 +0900

    change_final_maybe

--- web_des_html1/app.py
+++ web_des_html1/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template ,request
 import codecs
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
@@ -20,17 +21,19 @@ def go_skill():
 
 @app.route("/bbs.html",)
 def go_bbs():
-    file = codecs.open("static/article.txt", "r", "utf-8")
+    file = codecs.open("web_des_html1/static/article.txt", "r", "utf-8")
     lines = file.readlines()
     file.close()
-    return render_template("bbs.html",lines = lines,)
+    today = datetime.datetime.now()
+    date_now = today.strftime("%Y/%m/%d")
+    return render_template("bbs.html",lines = lines, date_now=date_now,)
 
 @app.route("/bbs.html/result", methods=["POST"])
 def result():
     article = request.form["article"]
     name = request.form["name"]
 
-    file = codecs.open("static/article.txt", "a", "utf-8")
+    file = codecs.open("web_des_html1/static/article.txt", "a", "utf-8")
     file.write(article + "," + name + "\n")
     file.close()
-    return render_template("bbs_result.html", article = article, name = name)
+    return render_template("bbs_result.html", article = article, name = name,)
