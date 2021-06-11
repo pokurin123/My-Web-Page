35d69f75f203a5d4d0564bfea5e25ba241833561
Taiki Kimura <s1922074@stu.musashino-u.ac.jp>
2021-06-12 04:24:27 +0900

    newone

--- docs/js/fadein.js
+++ docs/js/fadein.js
@@ -4,7 +4,7 @@ $(function(){
         let imgPos = $(this).offset().top;
         let scroll = $(window).scrollTop();
         let windowHeight = $(window).height();
-        if (scroll > imgPos - windowHeight + 250){
+        if (scroll > imgPos - windowHeight + 500){
           $(this).addClass("fadein_scrollin");
         } else {
           $(this).removeClass("fadein_scrollin");
